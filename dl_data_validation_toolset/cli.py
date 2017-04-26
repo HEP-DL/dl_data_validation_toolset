c99586eef47b89e7e913a528464337aafecd2270
Kevin Wierman <kwierman@gmail.com>
2017-04-26 16:11:44 -0500

    Minor adjustments

--- dl_data_validation_toolset/cli.py
+++ dl_data_validation_toolset/cli.py
@@ -14,7 +14,9 @@ def main(files=None):
 @click.command()
 @click.argument('input_file', nargs=1)
 def validate_single_dl_file(input_file):
+  logging.basicConfig(level=logging.DEBUG)
   for test_case in BaseTest.__subclasses__():
+    logging.info("Testing: {}".format(test_case))
     logging.info(test_case)
     test_case(input_file).get_results()
 
